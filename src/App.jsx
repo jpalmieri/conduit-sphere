3ec4d3e6d621c9a94d7ce6d25d9bcf0129f8f53f
Joe Palmieri <drown.your.watches@gmail.com>
2026-01-13 22:11:39 -0800

    add depth of field effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Canvas } from '@react-three/fiber'
 import { OrbitControls, Environment } from '@react-three/drei'
+import { EffectComposer, DepthOfField, Bloom, Vignette } from '@react-three/postprocessing'
 import NoisySphere from './NoisySphere'
 
 function App() {
@@ -8,6 +9,11 @@ function App() {
       <Environment preset="city" background />
       <NoisySphere />
       <OrbitControls enablePan={false} enableZoom={true} />
+      <EffectComposer>
+        <DepthOfField focusDistance={0.025} focalLength={0.05} bokehScale={4} height={480} />
+        <Bloom luminanceThreshold={0.7} luminanceSmoothing={0.9} height={300} intensity={1.0} />
+        <Vignette eskil={false} offset={0.1} darkness={0.5} />
+      </EffectComposer>
     </Canvas>
   )
 }
